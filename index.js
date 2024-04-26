706f59d065cd7af69268ec48d7da4e389b469291
LOLA VAZQUEZ <47436085@ort>
2024-04-26 12:01:48 -0300

    post province

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import provincesArray from "./src/entities/province.js"
+import ValidacionesHelper from "./src/helpers/validaciones-helper.js";
 const app = express();
 const port = 3000; // El puerto 3000 (http://localhost:3000)
 // Agrego los Middlewares
@@ -12,10 +13,34 @@ app.get('/api/province', (req, res) => {
     res.status(200).send(resultado);
 })
 
-app.get('/api/province', (req, res) => { 
-    let id = req.params.id;
+app.get('/api/province/:id', (req, res) => { 
+    let Id = parseInt(req.params.id);
+    let province = provincesArray.find(p => p.id === Id);
+    if (province != null) res.status(200).send(province);
+    else res.status(404).send("No existe una provincia con ese ID");
+})
 
-    res.status(200).send(resultado);
+app.post('/api/province', (req, res) => {
+    const Id = ValidacionesHelper.getIntegerOrDefault(parseInt(req.query.id), null);
+    const Name = ValidacionesHelper.getStringOrDefault(req.query.name, null);
+    const Full_name = ValidacionesHelper.getStringOrDefault(req.query.full_name, null);
+    const Latitude = ValidacionesHelper.getIntegerOrDefault(parseInt(req.query.latitude), null);
+    const Longitude = ValidacionesHelper.getIntegerOrDefault(parseInt(req.query.longitude), null);
+    const Display_order = ValidacionesHelper.getIntegerOrDefault(req.query.display_order, null);
+    if (Id == null || Name == null || Full_name == null || Latitude == null || Longitude == null || Display_order == null) {
+        res.status(400).send("Faltan campos o son nulos");
+        }
+    else {
+        provincesArray.push(new {
+            id: Id, 
+            name: Name, 
+            full_name: Full_name, 
+            latitude: Latitude, 
+            longitude: Longitude, 
+            display_order: Display_order
+        });
+        res.status(201).send("Provincia creada satisfactoriamente");
+    }    
 })
 
 app.listen(port, () => {
--- src/helpers/validaciones-helper.js
+++ src/helpers/validaciones-helper.js
@@ -1,36 +1,12 @@
 class ValidacionesHelper {
     getIntegerOrDefault = (value, defaultValue) => {
-        if (isNaN(parseInt(value) || value == null)) return defaultValue;
-        return parseInt(value);
-};
+
+    if (typeof value === 'number') return value;
+    else return defaultValue;
+}
     getStringOrDefault = (value, defaultValue) => {
-        if (!isNaN(value) || value == null) return defaultValue;
-        return String(value);
-};
+    if (typeof value === 'string') return value;
+    else return defaultValue;
+}
 }
 export default new ValidacionesHelper();
-
-/*class ValidacionesHelper {
-
-    getIntegerOrDefault = (value, defaultValue) => {
-
-        if (typeof value !== 'number') {
-            return defaultValue;
-        } else if (Number.isInteger(valor)) {
-            return value;
-        }
-        return defaultValue;
-    }
-    getStringOrDefault = (value, defaultValue) => {
-        if (typeof value !== 'string'){
-            return defaultValue;
-        }
-        return value;
-    };
-};
-
-
-
-// Exporto todo lo que yo quiero exponer del módulo:
-export default new ValidacionesHelper();
-*/
